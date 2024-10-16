ef7a60d4711ec7b5eb4dafb1e9ea96cb1fbf5756
faialotaibi <f.alotaibi@tuwaiq.edu.sa>
2024-10-16 20:15:41 +0300

    app.py edited

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(os.environ.get(HOST), port=6379)
+cache = redis.Redis(os.environ.get("HOST"), port=6379)
 
 def get_hit_count():
     retries = 5
