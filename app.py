829bc4699098da57fe2325356f4f0f34a0650d0f
faialotaibi <f.alotaibi@tuwaiq.edu.sa>
2024-10-16 19:42:40 +0300

    environment variables added

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import time
+import os
 
 import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+cache = redis.Redis(os.environ.get(HOST), port=6379)
 
 def get_hit_count():
     retries = 5
